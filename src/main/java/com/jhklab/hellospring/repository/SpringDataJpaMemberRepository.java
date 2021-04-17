22508489bedd39e1774d988fe39752ce97455741
jhk920613 <926jhk@gmail.com>
2021-04-18 00:32:04 +0900

    0417  - 스프링 DB 접근 기술   . 스프링 데이터 JPA

---
+++ src/main/java/com/jhklab/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.jhklab.hellospring.repository;
+
+import com.jhklab.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
